930f84cb16585f0390176381dbae035ed5963b9b
MeschurovD <meschurovd@gmail.com>
2021-11-14 11:23:27 +0500

    reducer: completed codeSlice

--- src/reducer/codeSlice.ts
+++ src/reducer/codeSlice.ts
@@ -1,10 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { InitialStateType } from "./types/codeTypes";
+import { CodePanelType, InitialStateType } from "./types/codeTypes";
+import _uniqueId from 'lodash/uniqueId'
 
 const initialState: InitialStateType = {
   workSpace: [
     {
-      type: 'code'
+      id: _uniqueId(),
+      type: 'code',
+      code: ''
     }
   ]
 }
@@ -13,16 +16,74 @@ const codeSlice = createSlice({
   name: 'code',
   initialState,
   reducers: {
-    addCodePanel(state) {
-      state.workSpace.push({ type: 'code' })
+    addCodePanel(state, action) {
+      state.workSpace.push({
+        id: action.payload.id,
+        type: 'code',
+        code: ''
+      })
 
     },
-    addIframe(state) {
-      state.workSpace.push({type: 'iframe'})
+    addIframe(state, action) {
+      state.workSpace.push({
+        id: action.payload.id,
+        type: 'iframe',
+        code: undefined
+      })
     },
-    deleteItem() { }
+    deleteItem(state, action) {
+      const index: number = state.workSpace.findIndex(item => item.id === action.payload.id)
+      state.workSpace.splice(index, 1)
+    },
+    moveUp(state, action) {
+      const firstIndex: number = state.workSpace.findIndex(item => item.id === action.payload.id)
+      const secondIndex = firstIndex - 1
+     
+      const mas = state.workSpace.map((item, index) => {
+        if (index === firstIndex) {
+          return state.workSpace[secondIndex]
+        }
+        if (index === secondIndex) {
+          return state.workSpace[firstIndex]
+        }
+        return item
+      })
+     
+      //@ts-ignore
+      state.workSpace = mas
+    },
+    moveDown(state, action) {
+      const firstIndex: number = state.workSpace.findIndex(item => item.id === action.payload.id)
+      const secondIndex = firstIndex + 1
+     
+      const mas = state.workSpace.map((item, index) => {
+        if (index === firstIndex) {
+          return state.workSpace[secondIndex]
+        }
+        if (index === secondIndex) {
+          return state.workSpace[firstIndex]
+        }
+        return item
+      })
+     
+      //@ts-ignore
+      state.workSpace = mas
+    },
+    updateCode(state, action) {
+      const index = state.workSpace.findIndex(item => item.id === action.payload.id)
+      //@ts-ignore
+      state.workSpace[index].code = action.payload.code
+    }
   }
 })
 
-export const { addCodePanel, addIframe } = codeSlice.actions
+export const {
+  addCodePanel,
+  addIframe,
+  updateCode,
+  moveUp,
+  moveDown,
+  deleteItem
+} = codeSlice.actions
+
 export default codeSlice.reducer
--- src/reducer/types/codeTypes.ts
+++ src/reducer/types/codeTypes.ts
@@ -1,11 +1,14 @@
-interface CodePanelType {
+export interface CodePanelType {
+  id: string
   type: string
+  code: string | undefined
 }
 
 interface IframeType {
+  id: string
   type: string
 }
 
 export interface InitialStateType {
-  workSpace: (CodePanelType | IframeType)[]
+  workSpace: CodePanelType[]
 }
