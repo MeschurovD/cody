a64ebc1840b8863629415f644b35a80a8ebb3f03
djoker255-denis <meschuro4@gmail.com>
2021-11-25 14:52:24 +0500

    work-deploy-version

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash].js',
-    publicPath: '/'
+    publicPath: '/cody/'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
@@ -107,7 +107,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
       "process.env": Dotenv.parsed
     }),
